1aaa37304b5af4685da5894e90456e9eafaf53ee
Mark Momot <markmomot03@gmail.com>
2022-06-01 20:57:30 -0400

    commit tree 79e7d06b95bafda5c3d986529b774217ce1dd5a8

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
 export function manageFriends(state, action) {
-  // your code here
+  const updatedState = [...state.friends]
+
+  switch (action.type){
+    case "friends/add":
+      updatedState.push(action.payload)
+      return{friends: updatedState}
+    case "friends/remove":
+      return{friends: updatedState.filter(friend => {friend.id !== action.payload})}
+    default:
+      return state
+  }
 }
